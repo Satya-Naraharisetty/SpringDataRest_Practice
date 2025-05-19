290d914994521dc32bc55cdb7f64e62a6a920344
Satya Naraharisetty <venkatnaraharisetty1234@gmail.com>
2025-05-19 19:41:31 +0530

    .

--- src/main/java/com/sbproj/springdatarest/model/JobPost.java
+++ src/main/java/com/sbproj/springdatarest/model/JobPost.java
@@ -1,18 +1,17 @@
 package com.sbproj.springdatarest.model;
 
-
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
+@Entity
 public class JobPost {
 	@Id
 	private int postId;
